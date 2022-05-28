ce0c5b033bece919bac33b05989b34450df2635b
Jehan <jehan.debryas@student-cs.fr>
2022-05-28 20:40:39 +0200

    electrical bicycle coded

--- GroupR_MyVelib_DeBryas_Gopee/src/myVelib/ElectricalBicycle.java
+++ GroupR_MyVelib_DeBryas_Gopee/src/myVelib/ElectricalBicycle.java
@@ -1,5 +1,50 @@
 package myVelib;
 
-public class ElectricalBicycle {
+import java.util.*;
+import java.lang.*;
+
+
+public class ElectricalBicycle extends Bicycle {
+	
+	private UUID ID;
+	private int currentRideTime;
+	private ArrayList<History> histories;
+	
+	public ElectricalBicycle(){
+		this.setID(UUID.randomUUID());
+	}
+
+	@Override
+	public double accept(RegistrationCard rc) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public UUID getID() {
+		return ID;
+	}
+
+	public void setID(UUID iD) {
+		ID = iD;
+	}
+
+	public int getCurrentRideTime() {
+		return currentRideTime;
+	}
+
+	public void setCurrentRideTime(int currentRideTime) {
+		this.currentRideTime = currentRideTime;
+	}
+
+	public ArrayList<History> getHistories() {
+		return histories;
+	}
+
+	public void setHistories(ArrayList<History> histories) {
+		this.histories = histories;
+	}
+	
+	
+	
 
 }
