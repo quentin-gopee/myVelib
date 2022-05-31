7d5435b83014ccc2f40872701bd56b9d941be8f3
Jehan <jehan.debryas@student-cs.fr>
2022-05-31 18:19:59 +0200

    Plan test

---
+++ GroupR_MyVelib_DeBryas_Gopee/src/myVelib/PlanTest.java
@@ -0,0 +1,15 @@
+package myVelib;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlanTest {
+
+	@Test
+	void testClassicPlan() {
+		
+		fail("Not yet implemented");
+	}
+
+}
